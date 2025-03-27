5a1edc4a939e33b5ff2e7f277cdb23a2651d2b3f
sean watters <sean@watters.io>
2025-03-26 19:49:23 -0700

    adds encrypt_dh and generate_dh_keys

--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,71 @@ pub fn generate_fingerprint() -> FingerprintResult {
         verifier: verifier.into(),
     }
 }
+
+#[wasm_bindgen]
+pub struct DhKeysResult {
+    private: Vec<u8>,
+    public: Vec<u8>,
+}
+
+#[wasm_bindgen]
+impl DhKeysResult {
+    #[wasm_bindgen(getter)]
+    pub fn private(&self) -> Vec<u8> {
+        self.private.clone()
+    }
+    #[wasm_bindgen(getter)]
+    pub fn public(&self) -> Vec<u8> {
+        self.public.clone()
+    }
+}
+
+#[wasm_bindgen]
+pub fn generate_dh_keys() -> DhKeysResult {
+    let (private, public) = rgp::generate_dh_keys();
+
+    DhKeysResult {
+        private: private.into(),
+        public: public.into(),
+    }
+}
+
+#[wasm_bindgen]
+pub fn encrypt_dh(
+    fingerprint: Vec<u8>,
+    content: Vec<u8>,
+    private_key: Vec<u8>,
+    public_keys: Vec<u8>,
+) -> Result<Vec<u8>, String> {
+    let fixed_fingerprint: [u8; 32] = match fingerprint.try_into() {
+        Ok(f) => f,
+        Err(_) => return Err("failed to convert fingerprint to 32 len array".into()),
+    };
+
+    let fixed_private_key: [u8; 32] = match private_key.try_into() {
+        Ok(f) => f,
+        Err(_) => return Err("failed to convert private_key to 32 len array".into()),
+    };
+
+    let mut fixed_public_keys = vec![];
+
+    for public_key in public_keys.chunks_exact(32) {
+        let fixed_public_key: [u8; 32] = match public_key.try_into() {
+            Ok(f) => f,
+            Err(_) => return Err("failed to convert public_key to 32 len array".into()),
+        };
+
+        fixed_public_keys.push(fixed_public_key);
+    }
+
+    let encrypted_content = match rgp::encrypt(
+        fixed_fingerprint,
+        content.clone(),
+        rgp::Encrypt::Dh(fixed_private_key, &fixed_public_keys, None),
+    ) {
+        Ok((encrypted_content, _)) => encrypted_content,
+        Err(err) => return Err(err.into()),
+    };
+
+    Ok(encrypted_content)
+}
